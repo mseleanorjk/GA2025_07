1eb6d6905b1cb8b341a993f0674a881b8e36c1e7
mcanela1 <m.canela.ramirez@umail.leidenuniv.nl>
2025-05-03 12:56:53 +0200

    Add functions to insert tables with sql magic code

--- HomeMessagesDB/home_messages_db.py
+++ HomeMessagesDB/home_messages_db.py
@@ -7,6 +7,52 @@ class HomeMessagesDB:
         self.url = url
 
     def create():
+        """Create Database if it doesn't exist"""
         if not FileExists(self.url):
             db = create_engine(self.url)
-    
+    def insert_table_smartthings():
+        """Create (if it doesnt exist) tables of smart things and devices"""
+        %%sql
+        CREATE TABLE IF NOT EXISTS devices (
+            name TEXT PRIMARY KEY,
+            loc TEXT NOT NULL,
+            level TEXT NOT NULL,
+            PRIMARY KEY (name)
+            FOREIGN KEY (name) 
+                REFERENCES smartthings (name)
+        )
+        %%sql
+        CREATE TABLE IF NOT EXISTS smartthings (
+            name TEXT NOT NULL,
+            epoch TEXT NOT NULL,
+            capability TEXT NOT NULL,
+            attribute TEXT NOT NULL,
+            value TEXT NOT NULL,
+            unit TEXT,
+            PRIMARY KEY (name,epoch,attribute)
+            FOREIGN KEY (name) 
+                REFERENCES devices (name)
+        )
+    def insert_table_p1e():
+        """Create p1e table if it does not exist"""
+        %%sql
+        CREATE TABLE IF NOT EXISTS p1e (
+            epoch INTEGER PRIMARY KEY,
+            ImportT1kWh FLOAT,
+            ImportT2kWh FLOAT,
+            ExportT1kWh FLOAT,
+            ExportT2kWh FLOAT,
+            PRIMARY KEY (epoch)
+            FOREIGN KEY (epoch) 
+                REFERENCES smartthings (epoch)
+        )
+    def insert_table_p1g():
+        """Create p1g table if it does not exist"""
+        %%sql
+        CREATE TABLE IF NOT EXISTS p1g (
+            epoch INTEGER PRIMARY KEY,
+            Total_gas_used FLOAT,
+            PRIMARY KEY (epoch)
+            FOREIGN KEY (epoch) 
+                REFERENCES smartthings (epoch)
+        )
