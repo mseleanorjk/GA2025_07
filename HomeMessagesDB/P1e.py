5a176e46eea541447ca485a7a6107a8677b4789c
mebgits <miro.eisenbarth.meb@gmail.com>
2025-05-15 16:32:20 +0200

    changed to using click library

--- HomeMessagesDB/P1e.py
+++ HomeMessagesDB/P1e.py
@@ -1,10 +1,7 @@
 import os
-import sys
 import click
-import argparse
 import glob
 import home_messages_db as db
-
 
 @click.command()
 @click.option('-d' , '--dburl', required = True, help = 'DBURL insert into the project database (DBURL is a SQLAlchemy database URL)')
@@ -13,19 +10,22 @@ def insert_file(dburl,filename):
     script_dir = os.path.dirname(os.path.realpath(__file__))
     main_dir = os.path.abspath(os.path.join(script_dir, '..'))
     p1e_dir = os.path.join(main_dir, 'data', 'P1e')
-    filename = filename
     full_path = os.path.join(p1e_dir, filename)
-    database = db.HomeMessagesDB(dburl)
+    files = glob.glob(full_path)
+    try:
+        database = db.HomeMessagesDB(dburl)
+    except Exception as e:
+        print(f"Error: {e}")
     try:
         database.create_db()
     except Exception as e:
         print(f"Error: {e}")
-    try:
-        database.insert_table_P1e(full_path)
-    except Exception as e:
-        print(f"Error: {e}")
-    print("Saving to:", os.path.abspath(dburl))
-    return(database)
+    for file in files:
+        try:
+            database.insert_table_P1e(file)
+        except Exception as e:
+            print(f"Error: {e}")
+        
 
 
 
